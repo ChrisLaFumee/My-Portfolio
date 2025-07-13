eb0f325f33650a4ece8e4244032f26bc9eb064b1
Chris LaFumee <chrislafumee@gmail.com>
2025-07-13 13:25:12 -0400

    new resume updates

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -11,7 +11,7 @@ export default function Resume() {
           className="block max-w-[200px] mx-auto"
         >
           <img
-            src="/resume-thumbnail.png"
+            src="/Tech-Resume.png"
             alt="Resume Preview"
             className="mt-4 w-full rounded-lg shadow-lg hover:opacity-80 transition"
           />
